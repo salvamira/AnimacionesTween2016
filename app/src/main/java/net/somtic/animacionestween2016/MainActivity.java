f270bf2b57112d85af4dfad175e57804a489d052
Salva Mira <smira@iesperemaria.com>
2016-09-30 08:28:56 +0200

    Initial commit

---
+++ app/src/main/java/net/somtic/animacionestween2016/MainActivity.java
@@ -0,0 +1,20 @@
+package net.somtic.animacionestween2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this,
+                R.anim.animacion);
+        texto.startAnimation(animacion);
+    }
+}
